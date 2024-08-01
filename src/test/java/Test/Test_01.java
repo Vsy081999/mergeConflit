91ebbbd32d59902c8736bc16c3c93f786b8ad555
Vishwanath Yadav <Vsy081999@gmail.com>
2024-08-02 01:04:31 +0530

    delete file

--- src/test/java/Test/Test_01.java
+++ src/test/java/Test/Test_01.java
@@ -2,14 +2,9 @@ package Test;
 
 public class Test_01 {
 
-
-public void name_1() {
-	System.out.println("name_1 of master branch");
+	public void name_1() {
+		System.out.println("master branch");
+	}
 }
 
 
-
-
-
-}
-	
